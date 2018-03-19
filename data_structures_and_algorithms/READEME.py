7965c2000fd24722c1923ad4fd8ad1d33e7d19c3
unknown <zhubiaook@outlook.com>
2018-03-19 21:46:03 +0800

    add data_structures_and_algorithms

--- data_structures_and_algorithms/READEME.py
+++ data_structures_and_algorithms/READEME.py
@@ -1,5 +1,6 @@
 '''算法重要性
 通过一个小时候常听到的高斯求和故事举例，使用恰当的算法，可以明显优化程序，减少运算时间
+比较使用循环求和与等差数列求和的运算时间
 '''
 import time
 
@@ -15,7 +16,9 @@ def sum2(n):
 	return (1+n)*n/2
 
 if __name__ == '__main__':
+	n = 99999999
 	start = time.time()
-	print(sum2(99999999))
+	#print(sum1(n))
+	print(sum2(n))
 	end = time.time()
 	print(end-start)
